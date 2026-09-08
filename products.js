2cdc33e71a97e2f222f6dc8b0ebdc694270d9159
talibiamine487-droid <talibiamine487@gmail.com>
2026-09-08 15:36:02 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,32 @@
+/*
+  RESPECT MASTER
+  لتعديل المتجر: غيّر فقط البيانات الموجودة هنا.
+  الصور: ضع ملفات الصور داخل مجلد images واكتب اسم الملف في image.
+*/
+const STORE = {
+  paypalUrl: "", // ضع رابط PayPal هنا لاحقاً
+
+  featured: [
+    { name: "CAR NAME #01", price: "€00", image: "images/car1.jpg", description: "وصف السيارة هنا" },
+    { name: "CAR NAME #02", price: "€00", image: "images/car2.jpg", description: "وصف السيارة هنا" },
+    { name: "CAR NAME #03", price: "€00", image: "images/car3.jpg", description: "وصف السيارة هنا" }
+  ],
+
+  cars: [
+    { name: "CAR NAME #04", price: "€00", image: "images/car4.jpg", description: "موديل • فئة • تفاصيل" },
+    { name: "CAR NAME #05", price: "€00", image: "images/car5.jpg", description: "موديل • فئة • تفاصيل" },
+    { name: "CAR NAME #06", price: "€00", image: "images/car6.jpg", description: "موديل • فئة • تفاصيل" }
+  ],
+
+  houses: [
+    { name: "HOUSE NAME #01", price: "€00", image: "images/house1.jpg", description: "وصف البيت ومميزاته" },
+    { name: "HOUSE NAME #02", price: "€00", image: "images/house2.jpg", description: "وصف البيت ومميزاته" },
+    { name: "HOUSE NAME #03", price: "€00", image: "images/house3.jpg", description: "وصف البيت ومميزاته" }
+  ],
+
+  bundles: [
+    { name: "STARTER PACK", price: "€00", image: "images/pack1.jpg", description: "محتويات الحزمة ومميزاتها" },
+    { name: "PREMIUM PACK", price: "€00", image: "images/pack2.jpg", description: "محتويات الحزمة ومميزاتها" },
+    { name: "ULTIMATE PACK", price: "€00", image: "images/pack3.jpg", description: "محتويات الحزمة ومميزاتها" }
+  ]
+};
