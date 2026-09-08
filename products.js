bf6993181a2867251fc823dc438c2e52a9256cad
talibiamine487-droid <talibiamine487@gmail.com>
2026-09-08 16:31:35 +0100

    Update featured product and convert prices to SAR

--- products.js
+++ products.js
@@ -2,26 +2,26 @@ const STORE = {
   paypalUrl: "https://www.paypal.com/ncp/payment/XVW2CC295GCMW", // رابط PayPal
 
   featured: [
-    { name: "CAR NAME #01", price: "€00", image: "images/car1.jpg", description: "وصف السيارة هنا" },
-    { name: "CAR NAME #02", price: "€00", image: "images/car2.jpg", description: "وصف السيارة هنا" },
-    { name: "CAR NAME #03", price: "€00", image: "images/car3.jpg", description: "وصف السيارة هنا" }
+    { name: "tcharger", price: "75 ر.س", image: "images/car1.jpg", description: "تشيل 4 و سرعتها 235" },
+    { name: "CAR NAME #02", price: "0 ر.س", image: "images/car2.jpg", description: "وصف السيارة هنا" },
+    { name: "CAR NAME #03", price: "0 ر.س", image: "images/car3.jpg", description: "وصف السيارة هنا" }
   ],
 
   cars: [
-    { name: "CAR NAME #04", price: "€00", image: "images/car4.jpg", description: "موديل • فئة • تفاصيل" },
-    { name: "CAR NAME #05", price: "€00", image: "images/car5.jpg", description: "موديل • فئة • تفاصيل" },
-    { name: "CAR NAME #06", price: "€00", image: "images/car6.jpg", description: "موديل • فئة • تفاصيل" }
+    { name: "CAR NAME #04", price: "0 ر.س", image: "images/car4.jpg", description: "موديل • فئة • تفاصيل" },
+    { name: "CAR NAME #05", price: "0 ر.س", image: "images/car5.jpg", description: "موديل • فئة • تفاصيل" },
+    { name: "CAR NAME #06", price: "0 ر.س", image: "images/car6.jpg", description: "موديل • فئة • تفاصيل" }
   ],
 
   houses: [
-    { name: "HOUSE NAME #01", price: "€00", image: "images/house1.jpg", description: "وصف البيت ومميزاته" },
-    { name: "HOUSE NAME #02", price: "€00", image: "images/house2.jpg", description: "وصف البيت ومميزاته" },
-    { name: "HOUSE NAME #03", price: "€00", image: "images/house3.jpg", description: "وصف البيت ومميزاته" }
+    { name: "HOUSE NAME #01", price: "0 ر.س", image: "images/house1.jpg", description: "وصف البيت ومميزاته" },
+    { name: "HOUSE NAME #02", price: "0 ر.س", image: "images/house2.jpg", description: "وصف البيت ومميزاته" },
+    { name: "HOUSE NAME #03", price: "0 ر.س", image: "images/house3.jpg", description: "وصف البيت ومميزاته" }
   ],
 
   bundles: [
-    { name: "STARTER PACK", price: "€00", image: "images/pack1.jpg", description: "محتويات الحزمة ومميزاتها" },
-    { name: "PREMIUM PACK", price: "€00", image: "images/pack2.jpg", description: "محتويات الحزمة ومميزاتها" },
-    { name: "ULTIMATE PACK", price: "€00", image: "images/pack3.jpg", description: "محتويات الحزمة ومميزاتها" }
+    { name: "STARTER PACK", price: "0 ر.س", image: "images/pack1.jpg", description: "محتويات الحزمة ومميزاتها" },
+    { name: "PREMIUM PACK", price: "0 ر.س", image: "images/pack2.jpg", description: "محتويات الحزمة ومميزاتها" },
+    { name: "ULTIMATE PACK", price: "0 ر.س", image: "images/pack3.jpg", description: "محتويات الحزمة ومميزاتها" }
   ]
 };
